177c10248c8b50a4f629b8c0270c63324a03a4e8
duyducpm0120 <ducduypm0120@gmail.com>
2021-11-19 23:23:52 +0700

    add cors

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const express = require ('express');
-const firebaseAdmin = require ('firebase-admin');
-const apiKey = require ('./APIkeys/apiKey.json');
-const AuthRouter = require ('./routers/AuthRouter');
-const authenticate = require ('./authenticate');
-const ProfileRouter = require ('./routers/ProfileRouter');
-const storageBucket = require ('./APIKeys/storageBucket');
-const CandidateTripRouter = require ('./routers/CandidateTripRouter');
-const NotificationRouter = require ('./routers/NotificationRouter');
-firebaseAdmin.initializeApp ({
-  credential: firebaseAdmin.credential.cert (apiKey),
+const express = require("express");
+const firebaseAdmin = require("firebase-admin");
+const apiKey = require("./APIkeys/apiKey.json");
+const AuthRouter = require("./routers/AuthRouter");
+const authenticate = require("./authenticate");
+const ProfileRouter = require("./routers/ProfileRouter");
+const storageBucket = require("./APIKeys/storageBucket");
+const CandidateTripRouter = require("./routers/CandidateTripRouter");
+const NotificationRouter = require("./routers/NotificationRouter");
+var cors = require("cors");
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(apiKey),
   storageBucket: storageBucket,
 });
 
-const app = express ();
-app.use (express.json ());
-app.use (express.urlencoded ({extended: true}));
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use ('/auth', AuthRouter);
-app.use ('/notification', NotificationRouter);
+app.use("/auth", AuthRouter);
+app.use("/notification", NotificationRouter);
 
+app.use(authenticate);
 
-app.use (authenticate);
+app.use("/profile", ProfileRouter);
 
-app.use ('/profile', ProfileRouter);
+app.use("/candidatetrip", CandidateTripRouter);
 
-app.use ('/candidatetrip', CandidateTripRouter);
-
-app.listen (3001);
+app.listen(3001);
